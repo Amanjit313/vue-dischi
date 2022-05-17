f2847c05f599e04c5edd5fd52f25ab17e1261de5
Amanjit313 <amanjitsingh313@gmail.com>
2022-05-17 17:22:12 +0200

    caricati i miei dati correttamente, manca la stampa

---
+++ src/components/myMain.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    MAIN
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "myMain",
+  data(){
+    return{
+      dataURL: "https://flynn.boolean.careers/exercises/api/array/music",
+      musics: [],
+      isLoaded: false
+    }
+  },
+  components: {
+  
+  },
+  
+  mounted(){
+    this.getAPI()
+  },
+
+  methods:{
+    getAPI(){
+     axios.get(this.dataURL)
+     .then(r => {
+        this.musics = r.data;
+        this.isLoaded = true
+     })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
